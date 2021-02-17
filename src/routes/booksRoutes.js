ecc78dd56393cd62ae29ea598d106f6fc985313b
Bruno Mendes <brunomendes962@gmail.com>
2021-02-17 19:05:32 -0200

    Implementando Rota add Book

--- src/routes/booksRoutes.js
+++ src/routes/booksRoutes.js
@@ -3,8 +3,6 @@ const { openDbConnection, closeDbConnection } = require('../models/connection')
 const userBooksSchema = require('../models/userBooksSchema')
 
 
-
-
 booksRouter.get('/all', async (request, response)=> {
   const { username } = request
   openDbConnection()
@@ -15,6 +13,27 @@ booksRouter.get('/all', async (request, response)=> {
   } catch(err) {
     response.status(500).json()
   }
+  response.json(request.user)
+})
+
+booksRouter.post('/add', async (request, response)=> {
+  const { username } = request
+  const { book } = request.body
+
+  try {
+    openDbConnection()
+    const user = await userBooksSchema.findOne({username}) 
+    user.books.push(book)
+    const save = await user.save()
+    response.status(201).json(user.books)
+    console.log(user)
+
+  } catch(err) {
+    response.status(500).json(err.message)
+  }
+
+  closeDbConnection()
+
   response.json(request.user)
 })
 
