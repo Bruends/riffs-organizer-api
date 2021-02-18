b0881a27ec014bc494b2534827465112a2b61ca4
Bruno Mendes <brunomendes962@gmail.com>
2021-02-18 12:36:02 -0200

    refatorando user routes com user controllers

---
+++ src/controllers/userController.js
@@ -0,0 +1,67 @@
+const { openDbConnection, closeDbConnection } = require('../models/connection')
+const UserBooksSchema = require('../models/userBooksSchema')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+
+// LOGIN
+const login = async (request, response) => {
+  let user 
+  try {
+    openDbConnection()
+    const { username, password } = request.body   
+    user = await UserBooksSchema.findOne({username})
+    
+    if(user) {
+        const isPasswordMatch = await bcrypt.compare(password, user.password)
+
+      // verificando senha
+      if(isPasswordMatch){
+        const token = jwt.sign(user.username, process.env.JWT_SECRET)
+        response.status(200).json({token, username: user.username})
+      } else {
+        response.status(403).json({error: 'senha incorreta'})
+      }
+
+    }  else {
+      response.status(403).json({error: 'Usuario não encontrado'})
+    }
+  
+  } catch (err) {
+    response.status(500).json({error: err.message})
+  
+  } finally {
+    closeDbConnection()
+  }
+}
+
+// ADICIONAR USUARIO
+const addUser = async (request, response) => {
+  const {username, email, password} = request.body
+
+  
+  try {
+    const user = new UserBooksSchema({
+      username,
+      email,
+      password
+    })  
+
+    openDbConnection()
+    // hash
+    user.password = await bcrypt.hash(user.password, 10)
+
+    await user.save()
+    response.status(201).json({})
+
+  } catch (err){
+    response.status(500).json({message: err.message})
+
+  } finally {
+    closeDbConnection()
+  }
+}
+
+module.exports = {
+  login,
+  addUser
+}
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,64 +1,14 @@
 const userRouter = require('express').Router()
-const { openDbConnection, closeDbConnection } = require('../models/connection')
-const bcrypt = require('bcrypt')
-const userBooksSchema = require('../models/userBooksSchema')
-const jwt = require('jsonwebtoken')
+const userController = require('../controllers/userController')
 
 // LOGIN
 userRouter.post('/login', async (request, response) => {
-  let user 
-  const con = openDbConnection()
-  try {
-    const { username, password } = request.body
-   
-    const user = await userBooksSchema.findOne({username})
-    console.log(user)    
-
-    if(user) {
-        const isPasswordMatch = await bcrypt.compare(password, user.password)
-      // verificando senha
-      if(isPasswordMatch){
-        const token = jwt.sign(user.username, process.env.JWT_SECRET)
-        response.status(200).json({token, username: user.username})
-      } else {
-        response.status(403).json({error: 'senha incorreta'})
-      }
-
-    }  else {
-      response.status(403).json({error: 'Usuario não encontrado'})
-    }
-  
-  } catch (err) {
-    response.status(500).json({error: err.message})
-  
-  } finally {
-    closeDbConnection()
-  }
+  userController.login(request, response)
 })
 
 // ADICIONAR USUARIO
 userRouter.post('/adduser', async (request, response) => { 
-  openDbConnection()
-
-  const user = new UserBooksSchema({
-    username: request.body.username,
-    email: request.body.email,
-    password: request.body.password
-  })
-
-  try {
-    // hash
-    user.password = await bcrypt.hash(user.password, 10)
-
-    const users = await user.save()
-    response.status(201).json(users)
-
-  } catch (err){
-    response.status(400).json({message: err.message})
-
-  } finally {
-    closeDbConnection()
-  }
+  userController.addUser(request, response)
 })
 
 module.exports = userRouter
